b6609bea67c83d2f557fd346dc49c5285a18b0b1
GallonTea <gallon0916@gmail.com>
2022-11-05 14:51:49 +0800

    Add FuckBranch

--- GitSample/src/nmsl/MainApp.java
+++ GitSample/src/nmsl/MainApp.java
@@ -4,6 +4,13 @@ public class MainApp {
 	
 	public static void main(String[] args) {
 		System.out.print("NMSL");
+		Math cMath = new Math();
+		System.out.println(cMath.mul(2, 6));
 	}
 
+}
+class Math{
+	int mul(int x, int y) {
+		return x * y;
+	}		
 }
